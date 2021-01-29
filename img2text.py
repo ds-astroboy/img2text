e3e795a635f34dd30cda666bd2503d10faa79257
ds-astroboy <davidsanter397@gmail.com>
2021-01-29 00:21:40 +0000

    Comment img2text.py for clarity. Adjust help comment for -w

--- img2text.py
+++ img2text.py
@@ -13,25 +13,31 @@ parser.add_argument("input_file", type=str, help="path to original image")
 parser.add_argument("colors", type=int, help="number of grayscale values to use on output")
 parser.add_argument("output_width", type=int, help="width of output in characters")
 parser.add_argument("output_file", type=str, help="path to write text file")
-parser.add_argument("-w", "--web", action="store_true", help="use input_file as a URL")
+parser.add_argument("-w", "--web", action="store_true", help="use URL for input_file")
 args = parser.parse_args()
 
+# Handling http request for input_file if -w flag is present
 if args.web:
     original_img = Image.open(requests.get(args.input_file, stream=True).raw)
 else:
     original_img = Image.open(args.input_file)
+
+# Image processing:
+# Get dimensions
 original_width, original_height = original_img.size
-
+# Convert to grayscale -> reduce color palette
 img_bw_quantized = original_img.convert("L").quantize(colors=args.colors)
-
+# Rescale to account for distortion of pixels from squares -> rectangles
 scaling_factor = args.output_width / original_width
 processed_img = img_bw_quantized.resize((args.output_width, int(scaling_factor * original_height * char_aspect)))
-
+# Convert to numpy array
 img_array = np.array(processed_img)
 
+# Set characters for gradient ramp / ensure that color space spans whole gradient
 gradient = " .:-=+*#%@"
 usable_gradient = [int(round(i)) for i in np.linspace(0, len(gradient) - 1, args.colors)]
 
+# Write to file
 with open(args.output_file, "w") as f:
     for row in img_array:
         output = ""
